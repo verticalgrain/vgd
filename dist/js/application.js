429362cfbec8def0dbc3ac0f55ac92afbc58adb8
Jamie Graham <jamie@domain7.com>
2017-03-03 16:20:44 -0800

    Idea 2 - i like it and I'm going to keep it

--- dist/js/application.js
+++ dist/js/application.js
@@ -1,4 +1,4 @@
-/*! gruntyplate - v0.1.0 - 2017-03-02
+/*! gruntyplate - v0.1.0 - 2017-03-03
 * Copyright (c) 2017 Gruntyplate;*/
 
 app.overlay=function(a){"use strict";function b(){a(".js-overlay").click("on",function(){a(this).attr("data-overlay-type")}),a(".overlay").click("on",function(b){b.target===b.currentTarget&&(a("body").removeClass("is-overlay-open is-overlay-transparent"),a(".overlay").empty())})}a(window).on("load",b)}(jQuery);
